98e9b5301beb03fbc42517571de357837792ad1d
vishnu <vishnuvenugopal73@gmail.com>
2015-05-27 00:32:51 +0530

    dfg

--- ProjectJen/src/main/java/com/TestBeginner.java
+++ ProjectJen/src/main/java/com/TestBeginner.java
@@ -4,7 +4,7 @@ public class TestBeginner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Jenkinsdsdasd");
+		System.out.println("Hello Jexdnkinsdsdasd");
 
 	}
 
